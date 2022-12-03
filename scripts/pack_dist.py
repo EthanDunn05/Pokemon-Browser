300942083edfc8e3d13498b0fe58b1db4c58c090
Ethan Dunn <Ethan05Dunn@gmail.com>
2022-12-03 12:45:17 -0600

    Update mac dist and zip compression

--- scripts/pack_dist.py
+++ scripts/pack_dist.py
@@ -1,17 +1,25 @@
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 from glob import glob
 import os
+import stat
 
 distDir = './dist/pokemon-browser/'
 outDir = distDir + 'zips/'
 
-os.mkdir(outDir)
+try:
+  os.mkdir(outDir)
+except:
+ print('zips already exists')
+
+# Do mac stuff :/
+os.rename(distDir + 'pokemon-browser-mac_x64', distDir + 'pokemon-browser-mac_x64.app')
+os.chmod(distDir + 'pokemon-browser-mac_x64.app', stat.S_IXOTH)
 
 files = glob(distDir + 'pokemon-browser-*')
 
 for file in files:
   fileName = file.split('\\')[-1]
-  zipFile = ZipFile(outDir + fileName.split('.')[0] + '.zip', 'w')
+  zipFile = ZipFile(outDir + fileName.split('.')[0] + '.zip', 'w', ZIP_DEFLATED)
   
   zipFile.write(file, fileName)
   zipFile.write(distDir + 'resources.neu', 'resources.neu')
